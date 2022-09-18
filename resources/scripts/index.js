988c6cd94050788f7a9cd1879498f6a9e2fb62b7
Juilfsjc <64669923+Juilfsjc@users.noreply.github.com>
2022-09-18 11:14:42 -0400

    Updated Index html and js

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,13 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById("game-screen") ;
+const startGameButton = document.getElementById("start-game-button");
+const userName = document.getElementById("username");
+const userSelection = document.getElementById("user-selection");
+const goButton =  document.getElementById("go-button");
+const scoreParagraph = document.getElementById("score");
+const gameHistoryParagraph = document.getElementById("game-history") ;
+const resetGameButton = document.getElementById("reset-game-button")
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -16,27 +17,47 @@ gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-
+  scoreParagraph.innerText = `${game.username}:  ${game.score.user} v CPU: ${game.score.cpu}`
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
 
+  gameHistoryParagraph.innerText = game.gameHistoryLog.join("\n")
+
+}
+
+function resetGame(){
+  gameScreen.classList.add(`d-none`);
+  welcomeScreen.classList.remove(`d-none`);
+  updateScoreTallyUI()
+  updateGameHistoryUI()
 }
 
 // start-game-button EventListener
-startGameButton.addEventListener(`click`, function () {
-  const username = 
-  game = new RockPaperScissors(userName);
-  // Complete
+startGameButton.addEventListener(`click`, function (e) {
+  e.preventDefault()
+  game = new RockPaperScissors(userName.value);
+  welcomeScreen.classList.add("d-none")
+  gameScreen.classList.remove("d-none")
+  updateGameHistoryUI()
+  updateScoreTallyUI()
 });
 
 // go-button EventListener
-goButton.addEventListener(`click`, function () {
+goButton.addEventListener(`click`, function (e) {
+  e.preventDefault()
+  const choice = userSelection.options[userSelection.selectedIndex].text;
+  game.play(choice)
+  updateScoreTallyUI()
+  updateGameHistoryUI()
   
 });
 
-// If you're doing the extra-credit, uncomment the below: reset-game-button
-// resetGameButton.addEventListener(`click`, function(e) { 
+ resetGameButton.addEventListener(`click`, function(e) { 
+
+  resetGame()
+
+  e.preventDefault()
   
-// });
+ });
